7cbbe670b9fb74b53a53624b2c1fdd45c2ba0cea
Susan Potter <me@susanpotter.net>
2015-07-09 09:45:28 -0500

    openssl_1_0_2: update 1.0.2c -> 1.0.2d

--- pkgs/development/libraries/openssl/1.0.2.x.nix
+++ pkgs/development/libraries/openssl/1.0.2.x.nix
@@ -2,7 +2,7 @@
 , withCryptodev ? false, cryptodevHeaders }:
 
 let
-  name = "openssl-1.0.2c";
+  name = "openssl-1.0.2d";
 
   opensslCrossSystem = stdenv.lib.attrByPath [ "openssl" "system" ]
     (throw "openssl needs its platform name cross building" null)
@@ -44,7 +44,7 @@ stdenv.mkDerivation {
       "http://www.openssl.org/source/${name}.tar.gz"
       "http://openssl.linux-mirror.org/source/${name}.tar.gz"
     ];
-    sha256 = "10vasdg52qiyqvgbp14n9z7ghglmhzvag9qpiz2nfqssycvvlf00";
+    sha256 = "1j58r7rdj9fz2lanir8ajbx4bspb5jnm5ikl6dq8lql5fx43c737";
   };
 
   patches = (patchesCross false) ++ extraPatches;
